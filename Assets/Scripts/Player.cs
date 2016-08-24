55bd4eb9bb41a4d3826d78303232fb0589a97e26
Tin Huynh <tinlun123@gmail.com>
2016-08-24 02:54:09 -0700

    Added mouse movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    private Vector2 targetPosition;
+    
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Get mouse coordinate.
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+        // Right click to move.
+        if(Input.GetMouseButton(1)){
+            targetPosition = new Vector2(mouseX, mouseY);
+        }
+        lookAtMouse();
+        moveToward(targetPosition, speed);
+	}
+    
+    void lookAtMouse(){
+        // Get mouse coordinates.
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+        // Get the rotation that player needs to be in.
+        float rotation = Mathf.Atan2(mouseY - transform.position.y, mouseX - transform.position.x)*Mathf.Rad2Deg;
+        // Set the rotation.
+        transform.rotation = Quaternion.AngleAxis(rotation, Vector3.forward);
+        
+    }
+    
+    void moveToward(Vector2 targetPosition, float speed){
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 positionDifference = targetPosition - currentPosition;  // Vector between current and target position.
+        
+        // If moving won't move past target position:
+        if(positionDifference.magnitude > (positionDifference.normalized*speed).magnitude){
+            transform.position = currentPosition + (targetPosition - currentPosition).normalized*speed;
+        }
+        else{
+            transform.position = targetPosition;
+        }
+        
+    }
+}
